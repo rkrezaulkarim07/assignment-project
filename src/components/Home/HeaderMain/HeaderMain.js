8ea41eac06e958f12b9f1501447159b1b8f9c16a
MD Rezaul Karim <rkrezaulkarim07@gmail.com>
2021-05-09 17:23:03 +0600

    all done

---
+++ src/components/Home/HeaderMain/HeaderMain.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import Typical from 'react-typical'
+
+AOS.init({ duration: 2000 });
+
+const HeaderMain = () => {
+    return (
+        <div>
+            <div className="row d-flex">
+                <div className="col-md-6 text-center m-auto">
+                    <h1>Lorem ipsum dolor sit amet.</h1>
+                    <h5>Lorem ipsum dolor sit, amet
+                    <Typical steps={['ipsum dolor sit amet.', 'Lorem ipsum dolor sit amet.', 'ipsum dolor sit amet.']}
+                            loop={Infinity}
+                            wrapper="p"
+                        />
+                    </h5>
+                </div>
+                <div data-aos="slide-left" className="col-md-6">
+                    <img style={{ width: '100%' }} src="https://i.ibb.co/kH3sVKH/undraw-wireframing-nxyi.png" alt="" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default HeaderMain;
